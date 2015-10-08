cc8e02370f89f285302a176f6eace62d17872df7
wf <willfreyman@gmail.com>
2015-10-08 12:54:38 -0700

    final cloning

---
+++ spatial-data/2015-09-21-clone-CCH-geocodes/output/make_csv.sh
@@ -0,0 +1,11 @@
+python make_csv.py < CCH_clades_by_counties_Part_01.tsv > CCH_clades_by_counties_Part_01.csv
+python make_csv.py < CCH_clades_by_counties_Part_02.tsv > CCH_clades_by_counties_Part_02.csv
+python make_csv.py < CCH_clades_by_counties_Part_03.tsv > CCH_clades_by_counties_Part_03.csv
+python make_csv.py < CCH_clades_by_counties_Part_04.tsv > CCH_clades_by_counties_Part_04.csv
+python make_csv.py < CCH_clades_by_counties_Part_05.tsv > CCH_clades_by_counties_Part_05.csv
+python make_csv.py < CCH_clades_by_counties_Part_06.tsv > CCH_clades_by_counties_Part_06.csv
+python make_csv.py < CCH_clades_by_counties_Part_07.tsv > CCH_clades_by_counties_Part_07.csv
+python make_csv.py < CCH_clades_by_counties_Part_08.tsv > CCH_clades_by_counties_Part_08.csv
+python make_csv.py < CCH_clades_by_counties_Part_09.tsv > CCH_clades_by_counties_Part_09.csv
+python make_csv.py < CCH_clades_by_counties_Part_10.tsv > CCH_clades_by_counties_Part_10.csv
+python make_csv.py < CCH_clades_by_counties_Part_11.tsv > CCH_clades_by_counties_Part_11.csv
