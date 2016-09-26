e80ccc6dbb348e05e058c006105d906d4e397929
wf <willfreyman@gmail.com>
2016-09-25 22:02:03 -0700

    script to get taxonomic ranks of all genera

---
+++ world/2016-09-25-getting_taxonomic_ranks/get_taxonomic_ranks.R
@@ -0,0 +1,52 @@
+
+library(taxize)
+
+testing = TRUE
+taxids = read.csv("taxids.csv", header=FALSE, stringsAsFactors=FALSE)
+num_rows = nrow(taxids)
+
+data_out = data.frame(genus = character(),
+                      family = character(),
+                      order = character(),
+                      class = character(),
+                      clade = character(),
+                      stringsAsFactors=FALSE) 
+
+for (i in 1:num_rows) {
+
+    if (taxids[i, 2] != "not found") {
+        
+        ranks = classification(taxids[i, 2], db='ncbi')[[1]]
+        num_ranks = nrow(ranks)
+        genus = ""
+        family = ""
+        order = ""
+        class = ""
+        clade = ranks$name[9]
+
+        for (j in 9:num_ranks) {
+            
+            if (ranks$rank[j] == "class" || ranks$rank[j] == "subclass")
+                class = ranks$name[j]
+            if (ranks$rank[j] == "order")
+                order = ranks$name[j]
+            if (ranks$rank[j] == "family")
+                family = ranks$name[j]
+            if (ranks$rank[j] == "genus")
+                genus = ranks$name[j]
+
+        }
+
+        new_row = data.frame(genus = genus, 
+                             family = family,
+                             order = order,
+                             class = class,
+                             clade = clade)
+        data_out = rbind(data_out, new_row)
+    }
+
+    if (testing && i == 3)
+        break
+}
+
+write.csv(data_out, file = "taxonomic_ranks.csv", row.names = FALSE, quote = FALSE)
